50b1d06cc77fb256326366114621acb2164d8582
wfa <mrcwerfar@gmail.com>
2015-06-29 18:17:14 +0200

    Refactoring....

--- core/src/com/mygdx/game4/GameItem.java
+++ core/src/com/mygdx/game4/GameItem.java
@@ -5,19 +5,18 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.Rectangle;
 
-public class GameFigure extends Rectangle {
+public abstract class GameItem extends Rectangle {
 
-    private Sprite image;
+    protected Sprite image;
 
-    public GameFigure(int x, int y, int width, int height, String path) {
+    public GameItem(int x, int y, int width, int height, String path) {
         super(x, y, width, height);
 
         image = new Sprite(new Texture(Gdx.files.internal(path)));
     }
 
     //jfgfkjfg
-
-    public boolean overLaps(GameFigure object) {
+    public boolean overLaps(GameItem object) {
         return x < object.x + object.width &&
                 x + width > object.x &&
                 y < object.y + object.height &&
@@ -27,4 +26,7 @@ public class GameFigure extends Rectangle {
     public Sprite getImage() {
         return image;
     }
+
+    //Maa overrides i barneklassene:
+    public abstract void render();
 }
